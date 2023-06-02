cc7b1dba206f163c0f22168e87f4a1ae9ef2278a
Gilberto Mascena <gilbertomascena@gmail.com>
2023-06-01 22:28:16 -0300

    Starting repository

---
+++ if/src/ex01.java
@@ -0,0 +1,18 @@
+
+public class ex01 {
+	
+	/*
+	 * Contador com "for" para incremento. 
+	 * 
+	 * Estrutura repetitiva "para".
+	 * Usar quando se sabe previamente a quantidade de repetições.
+	 * Ótimo para fazer contagens(progressiva ou regressiva).
+	 */
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Valor de i: " + i);
+		}
+	}
+}
---
+++ maior/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double a, b;
+		
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		if (a > b) {
+			System.out.printf("O maior é: %.2f%n", a);
+		} 
+		else {
+			System.out.printf("O maior é: %.2f%n", b);
+		}		
+		
+		sc.close();
+		
+	}
+}
