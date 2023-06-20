f8bc795d3e671b60c08207d42988b3b833fdfde3
Gilberto Mascena <gilbertomascena@gmail.com>
2023-06-20 20:09:35 -0300

    Add new projects

---
+++ map/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Map<String, String> cookies = new TreeMap<>();
+
+		cookies.put("username", "Jose");
+		cookies.put("email", "jose@gmail.com");
+		cookies.put("phone", "229993351");
+
+		cookies.remove("email");
+		cookies.put("phone", "118882240");
+
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
+
+		System.out.println("ALL COOKIES: ");
+		for (String key : cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
+		}
+	}
+}
