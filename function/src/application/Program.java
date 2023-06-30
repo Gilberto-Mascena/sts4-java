152e5bdb7b398f4b72122457f36158a6d332d76e
Gilberto Mascena <gilbertomascena@gmail.com>
2023-06-30 19:40:20 -0300

    Add new projects, jdbc1 and jdbc2

--- function/src/application/Program.java
+++ function/src/application/Program.java
@@ -26,8 +26,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 
-		// Implementacao com interface.
-		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		/*
+		 * Implementacao com interface.
+		 * A funcao map() so funciona pra list.		  
+		 */
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());	
 
 		names.forEach(System.out::println);
 
